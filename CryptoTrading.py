fce109691387b3f67e1abe711f50924423da0735
shakerin <shakerin.ahmed@gmail.com>
2022-03-03 01:44:15 +0800

    added method for writing price to cells in excel

--- CryptoTrading.py
+++ CryptoTrading.py
@@ -3,15 +3,20 @@
 CryptoTrading.
 
 Usage:
-  CryptoTrading profit_estimate <ids_as_a_string>
+  CryptoTrading profit_estimate <ids_as_a_string> [--excel_file=<ef>]
 
 
 Options:
   <ids_as_a_string>         cryptocurrency ids to get info from coin
                             market cap API
+  
+  --excel_file=<ef>         path to the excel file that contains CRO,ADA calculation
+                            need to make it generalized later
 
 """
-import openpyxl
+from tkinter import E
+from numpy import save
+from openpyxl import Workbook, load_workbook
 from docopt import docopt
 from CryptoAnalyze import cryptocurrencyMap, cryptocurrencyQuotes
 
@@ -56,12 +61,34 @@ class ProfitEstimation(object):
         break
     return
 
+def profitEstimation(coin_1, coin_2):
+  pe = ProfitEstimation(coin_1, coin_2)
+  return
+
+
+def updateExcel(coin_1, coin_2, excel_file):
+  pe = ProfitEstimation(coin_1, coin_2)
+  workbook = load_workbook(excel_file)
+  output = excel_file.replace(".xlsx", "_2.xlsx")
+  sheet = workbook.worksheets[1]
+  sheet["G11"] = pe.coin_1_price
+  sheet["G12"] = pe.coin_2_price
+  workbook.save(output)
+  return
+
+
 def Main():
   args = docopt(__doc__, version='CryptoAnalyze 1.0')
   if (args["profit_estimate"]):
     coin_1 = args["<ids_as_a_string>"].split(",")[0]
     coin_2 = args["<ids_as_a_string>"].split(",")[1]
-    pe = ProfitEstimation(coin_1, coin_2)
+    if args["--excel_file"]:
+      print("updateExcel")
+      excel_file = args["--excel_file"] if args["--excel_file"]!="abc" else "/mnt/c/Users/shake/OneDrive/Desktop/coin market cap/cryptoInvestCalculator.xlsx"
+      updateExcel(coin_1, coin_2, excel_file)
+    else:
+      print("profitEstimation")
+      profitEstimation(coin_1, coin_2)
   return
 
 if __name__ == "__main__":
